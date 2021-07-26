e6f69a0af92cb87140a3b22ce7bcb5e59b7d83b6
Armand <kodjouarmand@otlook.com>
2021-07-26 15:50:50 +0100

    Update the Home page

--- BulkyBookV2/BulkyBookV2/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookV2/BulkyBookV2/Areas/Customer/Controllers/HomeController.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using BulkyBookV2.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using BulkyBookV2.Models;
+using BulkyBookV2.Models.ViewModels;
+using BulkyBookV2.DataAccess.Repository.IRepository;
 
-namespace BulkyBookV2.Area.Customer.Controllers
+namespace BulkyBookV2.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
