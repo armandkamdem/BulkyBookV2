08ad21dc6f87d709eed1b1ec1a9a94506b02229c
Armand <kodjouarmand@otlook.com>
2021-08-25 11:45:20 +0100

    Section 19 & 20

---
+++ BulkyBookV2/BulkyBookV2.Models/ViewModels/OrderDetailsVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBookV2.Models.ViewModels
+{
+    public class OrderDetailsVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetails> OrderDetails { get; set; }
+    }
+}
